b11df3c46bcd71924eb33a96d8a8e2c51b98080f
Harsh <harshtanishq2002@gmail.com>
2024-06-17 11:11:22 +0530

    feat : Functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('app is running');
+let greet;
+greet = () => {
+    console.log("hello there");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "50");
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(5, 2); // automatically assigned types
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-console.log('app is running');
+let greet: Function;
+
+greet = () => {
+  console.log("hello there");
+};
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10, "50");
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(5, 2); // automatically assigned types
